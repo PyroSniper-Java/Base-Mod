3d4f4996ba7b44ff1d75c798b0e3dfc92b514aea
PyroSniper-Java <k.matches@hotmail.com>
2024-09-06 19:01:47 -0400

    I Added Code

---
+++ src/main/java/com/pyrocoder/basemod/item/ModItems.java
@@ -0,0 +1,28 @@
+package com.pyrocoder.basemod.item;
+
+import com.pyrocoder.basemod.BaseMod;
+import com.pyrocoder.basemod.item.custom.WandItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+        DeferredRegister.create(ForgeRegistries.ITEMS, BaseMod.MOD_ID);
+
+    public static final RegistryObject<Item> STELLALUNITE = ITEMS.register("stellalunite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_STELLALUNITE = ITEMS.register("raw_stellalunite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STELLALUNITE_BONE = ITEMS.register("stellalunite_bone",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> WAND = ITEMS.register("wand",
+            () -> new WandItem(new Item.Properties().durability(32)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
